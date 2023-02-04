c91196d517fb2e0456116f726e67ddbc2a8597ec
Meysam Bavi <meybsky@gmail.com>
2023-02-05 00:01:04 +0330

    Add unit test for GetColors

---
+++ helper/info_test.go
@@ -0,0 +1,39 @@
+package helper_test
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"wordle-helper/helper"
+)
+
+type getColorsTestCase struct {
+	answer         string
+	guess          string
+	expectedColors string
+}
+
+func (g *getColorsTestCase) Test(t *testing.T) {
+	assert.Equal(t, g.expectedColors, helper.GetColors(g.answer, g.guess))
+}
+
+func TestGetColorsForFiveLetter(t *testing.T) {
+	testCases := []*getColorsTestCase{
+		{"kazoo", "loose", "byybb"},
+		{"unlit", "trace", "ybbbb"},
+		{"unlit", "innit", "bgbgg"},
+		{"unlit", "unlit", "ggggg"},
+		{"unlit", "unlit", "ggggg"},
+		{"cramp", "loose", "bbbbb"},
+		{"cramp", "chase", "gbgbb"},
+		{"cramp", "porge", "ybybb"},
+		{"cramp", "fleet", "bbbbb"},
+		{"cramp", "scrap", "byyyg"},
+		{"cramp", "crapy", "gggyb"},
+		{"cramp", "aaxyz", "ybbbb"},
+		{"cramp", "cramp", "ggggg"},
+	}
+
+	for _, testCase := range testCases {
+		testCase.Test(t)
+	}
+}
